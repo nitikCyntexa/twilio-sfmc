e691074e8a260c247d8c3e1dfa15a97de54e1740
MERATWALPRIYANKA3 <69080015+MERATWALPRIYANKA3@users.noreply.github.com>
2021-07-02 17:01:23 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -6,8 +6,8 @@ const Path = require('path');
 const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
 var http = require('https');
 //-----------------------------
-const SFClient = require('../utils/sfmc-client');//to save data in data extension
-const { v1: Uuidv1 } = require('uuid');   //for unique id
+//const SFClient = require('../utils/sfmc-client');//to save data in data extension
+//const { v1: Uuidv1 } = require('uuid');   //for unique id
 //----------------------------------------
 exports.logExecuteData = [];
 
