9e84a68b087df54011d8f8463cf405fa2945a526
MERATWALPRIYANKA3 <69080015+MERATWALPRIYANKA3@users.noreply.github.com>
2021-07-05 15:03:07 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -122,7 +122,7 @@ exports.execute = function (req, res) {
     try {
     const id = Uuidv1();
 
-   await function SFClient.saveData(process.env.DATA_EXTENSION_EXTERNAL_KEY, [
+   SFClient.saveData(process.env.DATA_EXTENSION_EXTERNAL_KEY, [
       {
         keys: {
           Id: id,
