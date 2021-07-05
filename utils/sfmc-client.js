445baaae0aa0eb4bac38ff3e57bd8f5a39379ead
MERATWALPRIYANKA3 <69080015+MERATWALPRIYANKA3@users.noreply.github.com>
2021-07-05 20:42:18 +0530

    Update sfmc-client.js

--- utils/sfmc-client.js
+++ utils/sfmc-client.js
@@ -23,7 +23,11 @@ const client = new FuelRest(options);
  * @param data
  * @returns {?Promise}
  */
-const saveData = function (externalKey, data) { client.post({
+const saveData = function (externalKey, data) { 
+  console.log('clientId:'+clientId);
+  console.log('clientSecret:'+clientSecret);
+  console.log('authUrl:'+authUrl);
+  client.post({
   uri: `/hub/v1/dataevents/key:${externalKey}/rowset`,
   headers: {
     'Content-Type': 'application/json',
